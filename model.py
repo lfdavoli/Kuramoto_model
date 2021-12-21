f77325ebe06727bbd0b6d016b9238c1cad81482e
lfdavoli <lorenzo.davoli3@studio.unibo.it>
2021-12-21 17:16:45 +0100

    Da commentare

--- model.py
+++ model.py
@@ -48,6 +48,9 @@ print(omegas)
 # notice dependence of final state
 
 # %% [markdown]
+# #### Considerations:
+
+# %% [markdown]
 # ### 3.
 
 # %%
@@ -76,14 +79,21 @@ for i,k in enumerate(K):
 
 
 # %%
+sol
+
+# %%
+sol.y[0][0]-sol.y[1][0]
+
+# %% [markdown]
+# ### 4.
+
+# %%
 N = 100
 K = np.linspace(1,2,25)
 t_max = 100
 N_sim = 100
 t = np.arange(1,t_max+1)
 
-
-fig,ax = plt.subplots(figsize=(15,10))
 R = {k:0 for k in K}
 
 for n_sim in np.arange(0,N_sim):
@@ -105,6 +115,77 @@ for n_sim in np.arange(0,N_sim):
 R_mean = [R[k] for k in K]
 plt.plot(R_mean)
 plt.grid()
+
+# %% [markdown]
+# ### 5.
+
+# %%
+N = [100,500,2000,5000,15000]
+N_rep = [300,100,50,30,10]
+K = np.linspace(1,2,25)
+t_max = 100
+t = np.arange(1,t_max+1)
+
+R_dict = {n:[0 for k in K] for n in N}
+
+for i,n in enumerate(N):
+    N_sim = N_rep[i]
+
+    for n_sim in np.arange(0,N_sim):
+        omegas = np.random.normal(size=n)
+        mean_omega = sum(omegas)/n
+        omegas = omegas - mean_omega
+        thetas = np.random.uniform(-np.pi,np.pi,size=n)
+
+        for i,k in enumerate(K):
+            sol = solve_ivp(dtheta_dt,[0,t_max],thetas,args=(omegas,k),t_eval=np.arange(0,t_max))
+            y = sol.y[:,50:]
+            sum_s = np.sum(np.sin(y),0)
+            sum_c = np.sum(np.cos(y),0)
+            psi = np.arctan(sum_s/sum_c)
+            r = np.mean(np.abs(sum_c/(n*np.cos(psi))))
+            R_dict[n][i]+=r/N_sim
+            
+
+# %%
+fig,ax = plt.subplots()
+for key in R_dict.keys():
+    ax.plot(K,R_dict[key],label='$N_{rotors} = $'+str(key))
+
+ax.legend()
+ax.grid()
+ax.set_title('Average R as function of K and rotors number')
+ax.set_xlabel('K')
+ax.set_ylabel('R')
+
+# %% [markdown]
+# ### 6.
+# Assuming normal distribution of omegas (CLT):
+# 
+# $p(0) = p(\omega=0)_{K} = \frac{1}{\sqrt{2 \pi}}$ 
+# 
+# $K_c = \frac{2}{\pi p_0}$
+
+# %%
+p_0 = 1/np.sqrt(2*np.pi)
+K_c = 2/(np.pi*p_0)
+K_c
+
+# %%
+p_0 = 1/np.sqrt(2*np.pi)
+K_c = 2/(np.pi*p_0)
+
+fig,ax = plt.subplots()
+for key in R_dict.keys():
+    ax.plot(K,R_dict[key],label='$N_{rotors} = $'+str(key))
+
+ax.axvline(x=K_c, label='$K_c$ threshold',color='g')
+
+ax.legend()
+ax.grid()
+ax.set_title('Average R as function of K and rotors number')
+ax.set_xlabel('K')
+ax.set_ylabel('R')
 
 # %%
 
